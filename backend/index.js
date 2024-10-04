b2f38a5256e35f7fb377d3c0fddecb526f8f0bb5
David Aparicio <davidaparicioj123@gmail.com>
2024-10-03 18:41:58 -0700

    Connected mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express"); // imports express library
 const app = express(); // creating instance of express app
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    mongoose.connect("mongodb://localhost:27017/e-comm");
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model("products", productSchema);
+    const data = await product.find();
+    console.warn(data);
+};
 
-// defines a route to handle get requests at the root ("/") URL
-app.get("/", (req, resp) => {
-    resp.send("app is working...")
-});
+connectDB();
 
 app.listen(5000); // localhost 5000
