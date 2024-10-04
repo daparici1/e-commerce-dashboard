8681519c92a2f3bc0c8983c74a066ac9e9651ee4
David Aparicio <davidaparicioj123@gmail.com>
2024-10-03 20:19:58 -0700

    Created database folder, implemented user signup API

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express = require("express"); // imports express library
+require("./database/config");
+const User = require("./database/User");
 const app = express(); // creating instance of express app
-const mongoose = require('mongoose');
-const connectDB = async () => {
-    mongoose.connect("mongodb://localhost:27017/e-comm");
-    const productSchema = new mongoose.Schema({});
-    const product = mongoose.model("products", productSchema);
-    const data = await product.find();
-    console.warn(data);
-};
 
-connectDB();
+app.use(express.json());
+
+// api for registration
+// reference notion to understand how all of this works
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body); // whatever we are getting from postman, we can store in our user database
+  let result = await user.save();
+  resp.send(result);
+});
 
 app.listen(5000); // localhost 5000
