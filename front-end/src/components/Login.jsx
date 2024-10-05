a784d7d4a31eba2e37e1870147621ba7804477fb
David Aparicio <davidaparicioj123@gmail.com>
2024-10-05 16:29:57 -0700

    Integrated login API; removed login from navbar while user is already logged in; user was able to bypass this by typing /login in address bar - fixed now

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,23 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const handleLogin = () => {
-        console.warn(email, password);
+  useEffect(() =>{
+    const auth = localStorage.getItem('user');
+    if (auth) {
+      navigate('/'); // REDACTS NAVIGATION, USER WAS STILL ABLE TO DO /login IN ADDRESS BAR
     }
+  }, []);
 
-    return(
-        <div>
-            <h1>Login</h1>
-            <input type="text" className='inputBox' placeholder='Enter Email' 
-            onChange={(e) => setEmail(e.target.value)} value={email}/>
-            <input type="password" className='inputBox' placeholder='Enter Password' 
-            onChange={(e) => setPassword(e.target.value)} value={password}/>
-            <button onClick={handleLogin} className='loginButton' type="button">Login</button>
-        </div>
-    );
+  const handleLogin = async () => {
+    let result = await fetch("http://localhost:5000/login", {
+      method: "post",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+
+    // we can see "no user found" in inspect, but the user cant, we can alert them
+    if (result.name) {
+      localStorage.setItem("user", JSON.stringify(result));
+      navigate("/");
+    } else {
+      alert("Please enter correct details");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        className="inputBox"
+        placeholder="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        className="inputBox"
+        placeholder="Enter Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button onClick={handleLogin} className="loginButton" type="button">
+        Login
+      </button>
+    </div>
+  );
 };
 
 export default Login;
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
     // local storage is how we track if someone is logged in or not
     // ...so clearing it would make it easy for us to have someone
     // ..."log out"
-    localStorage.clear(); 
-    navigate('/signup');
+    localStorage.clear();
+    navigate("/signup");
   };
   return (
     <div>
@@ -26,14 +26,24 @@ const Navbar = () => {
         <li>
           <Link to="/profile">Profile</Link>
         </li>
-        <li>
-          {auth ? <Link onClick={logout} to="/signup">Logout</Link>
-           : <Link to="/signup">Sign Up</Link>
-          }
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
+        {/* ternary operator fixes issue with login still showing up on navbar after user
+        has logged in */}
+        {auth ? (
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
