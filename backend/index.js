a874c348fa7230f064c55616f361ce7bc77c5276
David Aparicio <davidaparicioj123@gmail.com>
2024-10-04 18:02:38 -0700

    added login API with privacy conditionals

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,33 @@ const app = express(); // creating instance of express app
 app.use(express.json());
 app.use(cors());
 
-// api for registration
+// SIGNUP API
 // reference notion to understand how all of this works
 app.post("/register", async (req, resp) => {
   let user = new User(req.body); // whatever we are getting from postman, we can store in our user database
   let result = await user.save();
+  result = result.toObject();
+  delete result.password; // make sure password doesnt pop up in our output
   resp.send(result);
+});
+
+// LOGIN API
+app.post("/login", async (req, resp) => {
+  // conditional for check if the user inputs both email and password, 
+  // if only the email is inserted, we do not want to return anything
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      // conditional for checking if the user exists
+      resp.send(user);
+    } else {
+      resp.send({ result: "No user found" });
+    }
+  }
+  else {
+    resp.send({ result: "No user found "});
+  }
+  // notion page 14
 });
 
 app.listen(5000); // localhost 5000
