2c4ce3753637314a5afce6615582c573298bd9b3
David Aparicio <davidaparicioj123@gmail.com>
2024-10-07 00:38:53 -0700

    added autofill after user clicks on 'update product'

--- front-end/src/components/UpdateProduct.jsx
+++ front-end/src/components/UpdateProduct.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const params = useParams();
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    console.warn(params);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`);
+    result = await result.json();
+    console.warn(result);
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
 
   const updateProduct = async () => {
     console.warn(name, price, category, company);
-  }
+  };
 
   return (
     <div>
