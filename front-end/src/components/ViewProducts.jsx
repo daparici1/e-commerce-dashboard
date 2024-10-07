3fc69b4956ae4d0c875880531658828dd8de60ca
David Aparicio <davidaparicioj123@gmail.com>
2024-10-07 13:22:25 -0700

    added search bar to product view; integrated search product APIand created a conditional to produce 'no result found' when needed

--- front-end/src/components/ViewProducts.jsx
+++ front-end/src/components/ViewProducts.jsx
@@ -20,14 +20,32 @@ const ViewProducts = () => {
       method: "Delete",
     });
     result = await result.json();
-    if(result) {
-        getProducts();
+    if (result) {
+      getProducts();
+    }
+  };
+
+  const searchProduct = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
     }
   };
 
   return (
     <div>
       <h1>Product List:</h1>
+      <input
+        type="text"
+        placeholder="Search Product"
+        onChange={searchProduct}
+      />
       <ul>
         <li>S. No.</li>
         <li>Name</li>
@@ -35,18 +53,22 @@ const ViewProducts = () => {
         <li>Category</li>
         <li></li>
       </ul>
-      {products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>{item.price}</li>
-          <li>{item.category}</li>
-          <li>
-            <button onClick={() => deleteProduct(item._id)}>Delete</button>
-             <Link to={"/update/" + item._id}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>
+              <button onClick={() => deleteProduct(item._id)}>Delete</button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Result Found</h1>
+      )}
     </div>
   );
 };
